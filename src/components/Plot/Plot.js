5ff54214c7ab85d6a738e938906ef83a71304c02
CitralFlo <wojtas.michal90@gmail.com>
2023-03-29 17:18:16 +0200

    Add more plot projects to my site, cleanup React app files

--- src/components/Plot/Plot.js
+++ src/components/Plot/Plot.js
@@ -15,8 +15,9 @@ export const plots = [
         name: 'junglewoods',
         links: [
             'assets/plot-pictures/plot-junglewoods.png',
+            'assets/plot-pictures/plot-junglewoods-1.png',
             ],
-        desc: "Proste logo marki Vlone na podwyższeniu, podpalony netherrack nadaje działce trochę swagu"
+        desc: "Proste logo marki Vlone na podwyższeniu, a podpalony netherrack nadaje działce trochę swagu."
     },
     {
         serial: 2,
@@ -25,7 +26,39 @@ export const plots = [
             'assets/plot-pictures/plot-gothbrutality.png',
             'assets/plot-pictures/plot-gothbrutality-1.png',
             ],
-        desc: "Głowa własciciela działki z elementami netherracku, która przyzdabia wejście do strefy unikatów"
+        desc: "Głowa własciciela działki z elementami netherracku, która przyzdabia wejście do strefy unikatów."
+    },
+    {
+        serial: 1,
+        name: '1mph',
+        links: [
+            'assets/plot-pictures/plot-1mph.png',
+            'assets/plot-pictures/plot-1mph-1.png',
+            'assets/plot-pictures/plot-1mph-2.png',
+            ],
+        desc: "Działka ze spadem na środku przedstawia naturalne otoczenie oraz kamienną jaskinię prowadzącą dalej."
+    },
+    {
+        serial: 1,
+        name: 'hydroksyzine',
+        links: [
+            'assets/plot-pictures/plot-hydroksyzine.png',
+            'assets/plot-pictures/plot-hydroksyzine-1.png',
+            'assets/plot-pictures/plot-hydroksyzine-2.png',
+            'assets/plot-pictures/plot-hydroksyzine-3.png',
+            ],
+        desc: "Działka przedstawia pieknielną scenerią połączoną za napisem na bannerach podkreślającym range zawodniczki."
+    },
+    {
+        serial: 2,
+        name: 'kvdzi',
+        links: [
+            'assets/plot-pictures/plot-kvdzi.png',
+            'assets/plot-pictures/plot-kvdzi-1.png',
+            'assets/plot-pictures/plot-kvdzi-2.png',
+            'assets/plot-pictures/plot-kvdzi-3.png',
+            ],
+        desc: "Piekielna działka z instalacją wzorójącą się na kwarcowych żebrach oraz magicznych pilarach, przenosząca odwiedzającego do krainy netheru."
     }
 ]
 
