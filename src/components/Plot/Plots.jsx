6e04f421d9d8dc840716982f1e3919a45b61c3e7
CitralFlo <wojtas.michal90@gmail.com>
2023-04-05 18:26:07 +0200

    Add Popup screen to pictures for larger img sizes

--- src/components/Plot/Plots.jsx
+++ src/components/Plot/Plots.jsx
@@ -1,4 +1,5 @@
 import './plot.css';
+import PopupPicture from "./Popup/PopupPicture";
 import {plots} from './Plot.js';
 import React, {useState} from "react";
 
@@ -6,8 +7,7 @@ import React, {useState} from "react";
 function PlotSetup(props) {
     const item = props.item;
     const [currentPlot, setCurrentPlot] = useState(0);
-
-
+    const [isOpen, setIsOpen] = useState(false)
     const minPictures = 0, maxPictures = item.links.length - 1;
 
     const nextPlot = () => {
@@ -19,18 +19,33 @@ function PlotSetup(props) {
     };
     const displayCurrent = currentPlot + 1;
 
+
     return (
+
         <div className="single-plot">
             {currentPlot > minPictures && <button className="prev" onClick={prevPlot}>&lt;</button>}
             {currentPlot < maxPictures && <button className="next" onClick={nextPlot}>&gt;</button>}
 
 
-            <img src={item.links[currentPlot]} alt={item.name} className="picture"/>
-
+            <img src={item.links[currentPlot]} alt={item.name} className="picture" onClick={() => setIsOpen(true)}/>
 
             <label className="counter">{displayCurrent} / {maxPictures + 1}</label>
             <h1 className="plot-title">Działka {item.name} nr {item.serial} </h1>
             <h2 className="plot-desc">{item.desc}</h2>
+
+
+            <PopupPicture open={isOpen} onClose={() => setIsOpen(false)}>
+                {currentPlot > minPictures && <button className="prev" onClick={prevPlot}>&lt;</button>}
+                {currentPlot < maxPictures && <button className="next" onClick={nextPlot}>&gt;</button>}
+
+
+                <img src={item.links[currentPlot]} alt={item.name} className="popup-pic"
+                     onClick={() => setIsOpen(true)}/>
+
+                <label className="counter">{displayCurrent} / {maxPictures + 1}</label>
+                <h1 className="popup-title">{item.name}</h1>
+            </PopupPicture>
+
         </div>
     )
 }
---
+++ src/components/Plot/Popup/PopupPicture.js
@@ -0,0 +1,37 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+export default function PopupPicture({ open, children, onClose }) {
+    if (!open) return null;
+
+    return ReactDom.createPortal(
+        <>
+            <div style={overlay} onClick={onClose}/>
+            <div style={popup}>
+                {children}
+            </div>
+        </>,
+        document.getElementById('portal')
+    );
+}
+
+const overlay = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    zIndex: 1000,
+}
+
+const popup = {
+    position: 'fixed',
+    width: '80%',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '20px',
+    zIndex: 1000,
+    background: '#383838FF',
+}
