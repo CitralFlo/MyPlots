b354dc4b91849328557fe29d24a668b6ae625891
CitralFlo <wojtas.michal90@gmail.com>
2023-05-09 19:22:03 +0200

    Switch file format to .webp for faster loading speed

--- src/components/Plot/Plot.js
+++ src/components/Plot/Plot.js
@@ -3,9 +3,9 @@ export const plots = [
         serial: 1,
         name: 'waski021',
         links: [
-            'assets/plot-pictures/plot-waski021.png',
-            'assets/plot-pictures/plot-waski021-1.png',
-            'assets/plot-pictures/plot-waski021-2.png',
+            'assets/plot-pictures/plot-waski021.webp',
+            'assets/plot-pictures/plot-waski021-1.webp',
+            'assets/plot-pictures/plot-waski021-2.webp',
             ],
         desc: "Ta dzialka przedstawia proste igloo wykonane z bloków lodu, śniegu i wełny."
     },
@@ -13,8 +13,8 @@ export const plots = [
         serial: 1,
         name: 'junglewoods',
         links: [
-            'assets/plot-pictures/plot-junglewoods.png',
-            'assets/plot-pictures/plot-junglewoods-1.png',
+            'assets/plot-pictures/plot-junglewoods.webp',
+            'assets/plot-pictures/plot-junglewoods-1.webp',
             ],
         desc: "Proste logo marki Vlone na podwyższeniu, a podpalony netherrack nadaje działce trochę swagu."
     },
@@ -22,8 +22,8 @@ export const plots = [
         serial: 2,
         name: 'gothbrutality',
         links: [
-            'assets/plot-pictures/plot-gothbrutality.png',
-            'assets/plot-pictures/plot-gothbrutality-1.png',
+            'assets/plot-pictures/plot-gothbrutality.webp',
+            'assets/plot-pictures/plot-gothbrutality-1.webp',
             ],
         desc: "Głowa własciciela działki z elementami netherracku, która przyzdabia wejście do strefy unikatów."
     },
@@ -31,9 +31,9 @@ export const plots = [
         serial: 1,
         name: '1mph',
         links: [
-            'assets/plot-pictures/plot-1mph.png',
-            'assets/plot-pictures/plot-1mph-1.png',
-            'assets/plot-pictures/plot-1mph-2.png',
+            'assets/plot-pictures/plot-1mph.webp',
+            'assets/plot-pictures/plot-1mph-1.webp',
+            'assets/plot-pictures/plot-1mph-2.webp',
             ],
         desc: "Działka ze spadem na środku przedstawia naturalne otoczenie oraz kamienną jaskinię prowadzącą dalej."
     },
@@ -41,10 +41,10 @@ export const plots = [
         serial: 1,
         name: 'hydroksyzine',
         links: [
-            'assets/plot-pictures/plot-hydroksyzine.png',
-            'assets/plot-pictures/plot-hydroksyzine-1.png',
-            'assets/plot-pictures/plot-hydroksyzine-2.png',
-            'assets/plot-pictures/plot-hydroksyzine-3.png',
+            'assets/plot-pictures/plot-hydroksyzine.webp',
+            'assets/plot-pictures/plot-hydroksyzine-1.webp',
+            'assets/plot-pictures/plot-hydroksyzine-2.webp',
+            'assets/plot-pictures/plot-hydroksyzine-3.webp',
             ],
         desc: "Działka przedstawia pieknielną scenerią połączoną za napisem na bannerach podkreślającym range zawodniczki."
     },
@@ -52,10 +52,10 @@ export const plots = [
         serial: 2,
         name: 'kvdzi',
         links: [
-            'assets/plot-pictures/plot-kvdzi.png',
-            'assets/plot-pictures/plot-kvdzi-1.png',
-            'assets/plot-pictures/plot-kvdzi-2.png',
-            'assets/plot-pictures/plot-kvdzi-3.png',
+            'assets/plot-pictures/plot-kvdzi.webp',
+            'assets/plot-pictures/plot-kvdzi-1.webp',
+            'assets/plot-pictures/plot-kvdzi-2.webp',
+            'assets/plot-pictures/plot-kvdzi-3.webp',
             ],
         desc: "Piekielna działka z instalacją wzorójącą się na kwarcowych żebrach oraz magicznych pilarach, przenosząca odwiedzającego do krainy netheru."
     }
